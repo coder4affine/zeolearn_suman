15557da8253de899ae2908861fb89301ca034b63
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2019-07-02 07:54:57 +0530

    completed module part

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const data = "hello from app";
+
+export const data1 = "hello data1";
+
+export const data2 = "hello data2";
+
+export const data3 = "hello data3";
+
+export const data4 = "hello data4";
+
+export default data;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { data3 as data } from "./app";
+
+const greet = "hello";
+
+console.log(greet);
+
+console.log(data);
